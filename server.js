7ce21634c9404250f92df54ed4e24fc068ea4809
Abhiram Sanjeev Varier <abhiramv1551@gmail.com>
2025-01-27 19:33:51 +0530

    Project structure created - Ram

---
+++ server.js
@@ -0,0 +1,27 @@
+//imports
+//const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+
+//config
+dotenv.config();
+
+//rest object
+const app = express();
+
+//routes
+app.get("/", (req, res) => {
+  res.send("<h1>Node Server running on port 8080</h1>");
+});
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Node server running in ${process.env.DEV_MODE} mode on port no ${PORT}`
+      .bgGreen.white
+  );
+});
